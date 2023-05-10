f8a1460c6f58ba723ac93c5c3f7e1a9f2791606c
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-05-10 14:51:31 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const port = 3000
 
 const pokemon = require('./models/pokemon.js');
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('jsx-view-engine').createEngine());
+
 app.get('/', function(req, res){
     res.send('Welcome to the Pokemon App!');
 });   
 
 app.get('/pokemon', function(req, res){
-    res.send(pokemon);
+    res.render('Index', {pokemon: pokemon});
 });   
 
 
